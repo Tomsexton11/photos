866a83a7bf4d97ea20fc140d804a5ec48ae61cc4
Tomsexton11 <tom.sexton11@hotmail.com>
2018-07-22 16:09:29 +0100

    update to makecsv for others

--- makecsv.sh
+++ makecsv.sh
@@ -11,19 +11,63 @@ do
 	echo "./photos/steph$i.jpg;3" >> /home/pi/camcv/photos.csv
 done
 
-#change limits of for loops to select how many of 400 sample photos are used for 'others'
-for ((i=0; i<=0; i++))
-do
-	for ((j=0; j<=5; j++))
-	do
-		for ((k=0; k<=9; k++))
-		do
-
-			if [ ! $i -eq 0 ] || [ ! $j -eq 0 ] || [ ! $k -eq 0 ]; then	
-				echo "./photos/$i$j$k.jpg;0" >> /home/pi/camcv/photos.csv
-			fi
-		done
-	done
-done
-
-
+echo "./photos/others/001.jpg;0" >> /home/pi/camcv/photos.csv
+echo "./photos/others/002.jpg;0" >> /home/pi/camcv/photos.csv
+echo "./photos/others/003.jpg;0" >> /home/pi/camcv/photos.csv
+echo "./photos/others/004.jpg;0" >> /home/pi/camcv/photos.csv
+echo "./photos/others/005.jpg;0" >> /home/pi/camcv/photos.csv
+echo "./photos/others/006.jpg;0" >> /home/pi/camcv/photos.csv
+echo "./photos/others/007.jpg;0" >> /home/pi/camcv/photos.csv
+echo "./photos/others/008.jpg;0" >> /home/pi/camcv/photos.csv
+echo "./photos/others/009.jpg;0" >> /home/pi/camcv/photos.csv
+echo "./photos/others/010.jpg;0" >> /home/pi/camcv/photos.csv
+echo "./photos/others/011.jpg;0" >> /home/pi/camcv/photos.csv
+echo "./photos/others/012.jpg;0" >> /home/pi/camcv/photos.csv
+echo "./photos/others/013.jpg;0" >> /home/pi/camcv/photos.csv
+echo "./photos/others/014.jpg;0" >> /home/pi/camcv/photos.csv
+echo "./photos/others/015.jpg;0" >> /home/pi/camcv/photos.csv
+echo "./photos/others/016.jpg;0" >> /home/pi/camcv/photos.csv
+echo "./photos/others/017.jpg;0" >> /home/pi/camcv/photos.csv
+echo "./photos/others/018.jpg;0" >> /home/pi/camcv/photos.csv
+echo "./photos/others/019.jpg;0" >> /home/pi/camcv/photos.csv
+echo "./photos/others/020.jpg;0" >> /home/pi/camcv/photos.csv
+echo "./photos/others/021.jpg;0" >> /home/pi/camcv/photos.csv
+echo "./photos/others/022.jpg;0" >> /home/pi/camcv/photos.csv
+echo "./photos/others/023.jpg;0" >> /home/pi/camcv/photos.csv
+echo "./photos/others/024.jpg;0" >> /home/pi/camcv/photos.csv
+echo "./photos/others/025.jpg;0" >> /home/pi/camcv/photos.csv
+echo "./photos/others/026.jpg;0" >> /home/pi/camcv/photos.csv
+echo "./photos/others/027.jpg;0" >> /home/pi/camcv/photos.csv
+echo "./photos/others/028.jpg;0" >> /home/pi/camcv/photos.csv
+echo "./photos/others/029.jpg;0" >> /home/pi/camcv/photos.csv
+echo "./photos/others/030.jpg;0" >> /home/pi/camcv/photos.csv
+echo "./photos/others/031.jpg;0" >> /home/pi/camcv/photos.csv
+echo "./photos/others/032.jpg;0" >> /home/pi/camcv/photos.csv
+echo "./photos/others/033.jpg;0" >> /home/pi/camcv/photos.csv
+echo "./photos/others/034.jpg;0" >> /home/pi/camcv/photos.csv
+echo "./photos/others/035.jpg;0" >> /home/pi/camcv/photos.csv
+echo "./photos/others/036.jpg;0" >> /home/pi/camcv/photos.csv
+echo "./photos/others/037.jpg;0" >> /home/pi/camcv/photos.csv
+echo "./photos/others/038.jpg;0" >> /home/pi/camcv/photos.csv
+echo "./photos/others/039.jpg;0" >> /home/pi/camcv/photos.csv
+echo "./photos/others/040.jpg;0" >> /home/pi/camcv/photos.csv
+echo "./photos/others/041.jpg;0" >> /home/pi/camcv/photos.csv
+echo "./photos/others/042.jpg;0" >> /home/pi/camcv/photos.csv
+echo "./photos/others/043.jpg;0" >> /home/pi/camcv/photos.csv
+echo "./photos/others/044.jpg;0" >> /home/pi/camcv/photos.csv
+echo "./photos/others/045.jpg;0" >> /home/pi/camcv/photos.csv
+echo "./photos/others/046.jpg;0" >> /home/pi/camcv/photos.csv
+echo "./photos/others/047.jpg;0" >> /home/pi/camcv/photos.csv
+echo "./photos/others/048.jpg;0" >> /home/pi/camcv/photos.csv
+echo "./photos/others/049.jpg;0" >> /home/pi/camcv/photos.csv
+echo "./photos/others/050.jpg;0" >> /home/pi/camcv/photos.csv
+echo "./photos/others/051.jpg;0" >> /home/pi/camcv/photos.csv
+echo "./photos/others/052.jpg;0" >> /home/pi/camcv/photos.csv
+echo "./photos/others/053.jpg;0" >> /home/pi/camcv/photos.csv
+echo "./photos/others/054.jpg;0" >> /home/pi/camcv/photos.csv
+echo "./photos/others/055.jpg;0" >> /home/pi/camcv/photos.csv
+echo "./photos/others/056.jpg;0" >> /home/pi/camcv/photos.csv
+echo "./photos/others/057.jpg;0" >> /home/pi/camcv/photos.csv
+echo "./photos/others/058.jpg;0" >> /home/pi/camcv/photos.csv
+echo "./photos/others/059.jpg;0" >> /home/pi/camcv/photos.csv
+echo "./photos/others/060.jpg;0" >> /home/pi/camcv/photos.csv
